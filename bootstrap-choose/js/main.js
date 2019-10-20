c4481f23101b03f65fd9885e4b11dcb9e5cd0c18
kareem nour emam <karem_nour89@live.com>
2019-10-20 21:12:24 +0200

    v1

---
+++ bootstrap-choose/js/main.js
@@ -0,0 +1,9 @@
+$('.open-menu-icon').on('click', function (){
+    $('.main-nav-outer').addClass('open-menu');
+});
+
+$('.menu-close-icon').click(function (){
+    $('.main-nav-outer').removeClass('open-menu');
+});
+
+ 
